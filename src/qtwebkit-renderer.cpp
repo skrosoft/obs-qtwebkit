91f18331c9b1f91476193d5100032dfb4cbee60e
skrosoft <vguyard@onaxis.cl>
2016-11-22 02:34:37 -0300

    prueba

--- src/qtwebkit-renderer.cpp
+++ src/qtwebkit-renderer.cpp
@@ -102,24 +102,22 @@ int main(int argc, char *argv[])
 	int height = atoi(argv[3]);
 	int fps = atoi(argv[4]);
 	char *suffix = argv[5];
-	init_shared_data(atoi(argv[2]), atoi(argv[3]), suffix);
+	//init_shared_data(atoi(argv[2]), atoi(argv[3]), suffix);
 
 	QApplication app(argc, argv);
 	QWebPage page;
 
-	QWebPage newPage = page.createWindow(QWebPage::WebBrowserWindow);
-
-	QPalette palette = newPage.palette();
+	QPalette palette = page.palette();
 	palette.setBrush(QPalette::Base, Qt::transparent);
-	newPage.setPalette(palette);
-	newPage.settings()->setUserStyleSheetUrl(QUrl::fromUserInput(argv[6]));
-	newPage.settings()->setObjectCacheCapacities(0, 0, 0);
+	page.setPalette(palette);
+	page.settings()->setUserStyleSheetUrl(QUrl::fromUserInput(argv[6]));
+	page.settings()->setObjectCacheCapacities(0, 0, 0);
 
 	const QUrl url = QUrl::fromUserInput(argv[1]);
-	newPage.setViewportSize(QSize(width, height));
-	newPage.mainFrame()->setScrollBarPolicy(Qt::Horizontal, Qt::ScrollBarAlwaysOff);
-	newPage.mainFrame()->setScrollBarPolicy(Qt::Vertical, Qt::ScrollBarAlwaysOff);
-	newPage.mainFrame()->setUrl(url);
+	page.setViewportSize(QSize(width, height));
+	page.mainFrame()->setScrollBarPolicy(Qt::Horizontal, Qt::ScrollBarAlwaysOff);
+	page.mainFrame()->setScrollBarPolicy(Qt::Vertical, Qt::ScrollBarAlwaysOff);
+	page.mainFrame()->setUrl(url);
 
 	pthread_mutex_lock(&data->mutex);
 	QImage image(&data->data, width, height, QImage::Format_RGBA8888);
@@ -136,13 +134,13 @@ int main(int argc, char *argv[])
 
 		pthread_mutex_lock(&data->mutex);
 		image.fill(0);
-		newPage.mainFrame()->render(&painter, QWebFrame::ContentsLayer);
+		page.mainFrame()->render(&painter, QWebFrame::ContentsLayer);
 		pthread_mutex_unlock(&data->mutex);
 
 		// reload file if changed
 		if (refresh) {
 			refresh = 0;
-			newPage.mainFrame()->setUrl(url);
+			page.mainFrame()->setUrl(url);
 		}
 
 		usleep(1000000 / fps);
@@ -152,7 +150,7 @@ int main(int argc, char *argv[])
 		uninit_inotify();
 	}
 
-	uninit_shared_data(atoi(argv[2]), atoi(argv[3]));
+	//uninit_shared_data(atoi(argv[2]), atoi(argv[3]));
 
 	return 0;
 }
